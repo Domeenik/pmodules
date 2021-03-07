0d6f5e9be9caeb9533211c3029682b9160b211ea
d.reifer <d.reifer@tu-braunschweig.de>
2021-03-07 23:17:26 +0100

    added mqtt disconnect handler

--- mqtt.py
+++ mqtt.py
@@ -1,47 +1,14 @@
 import paho.mqtt.client as mqtt
 import json
-
-#ToDo: add general mqtt handler
-#ToDo: add rules
+import time
+import sys
 
 class MqttSender():
     def __init__(self, broker, port, topic, name):
         self.broker = broker
         self.port = port
         self.name = name
-        self.topic = topic
         
-        # remove '/' if there
-        if topic[-1] == "/":
-            self.topic = self.topic[:-1]
-
-        self.connect()
-
-    def connect(self):
-        #TODO change to mac adress related name
-        self.client = mqtt.Client(self.name)
-        self.client.on_publish = self.on_publish
-        self.client.connect(self.broker, self.port)
-        print("[INFO] connected to mqtt server {}:{} on {} as sender: {}".format(self.broker, self.port, self.topic, self.name))
-
-    def send(self, payload, topic="", name=""):
-        payload = json.dumps(payload)
-        if not topic == "":
-            return self.client.publish('{}/{}'.format(topic, name), payload)
-        else:
-            return self.client.publish('{}/{}'.format(self.topic, self.name), payload)
-
-    def on_publish(self, client, userdata, result):
-        return result
-
-
-class MqttListener():
-    def __init__(self, broker, port, topic, name="*", function=None):
-        self.broker = broker
-        self.port = port
-        self.name = name
-        self.function = function
-
         # set topic
         if topic[-1] == "/":
             self.topic = '{}{}'.format(topic, self.name)
@@ -50,15 +17,73 @@ class MqttListener():
 
         self.connect()
 
-    def on_message(self, client, userdata, msg):
-        self.function(msg)
-
-    def on_connect(self, client, userdata, flags, rc):
-        self.client.subscribe(self.topic)
-
     def connect(self):
+        #TODO change to mac adress related name
+        self.clientname = self.name + "_" + str(time.localtime().tm_min) + str(time.localtime().tm_sec)
+        self.client = mqtt.Client(self.clientname)
+        self.client.on_publish = self.on_publish
+        self.client.on_disconnect = self.on_disconnect
+        rc = self.client.connect(self.broker, self.port)
+        if rc == 0:
+            print("[MQTT] connected to mqtt server {}:{} on {}".format(self.broker, self.port, self.topic))
+
+    def send(self, payload, topic=""):
+        payload = json.dumps(payload)
+        if not topic == "":
+            return self.client.publish(topic, payload)
+        else:
+            return self.client.publish(self.topic, payload)
+
+    def on_publish(self, client, userdata, result):
+        return result
+
+    def on_disconnect(self, userdata, rc):
+        print[f"[MQTT] disconnected: {str(rc)}"]
+        print("[MQTT] trying to reconnect ...")
+        self.client.reconnect()
+
+
+class MqttListener():
+    def __init__(self, broker, port, topic, function):
+        self.broker = broker
+        self.port = port
+        self.topic = topic
+        self.function = function
+        self.timeout = 10
+
+        # create client object
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-        self.client.connect(self.broker, self.port)
-        self.client.loop_start()
+        self.client.on_disconnect = self.on_disconnect
+
+        self.connect()
+
+    def connect(self):
+        rc = self.client.connect(self.broker, self.port, self.timeout)
+        if rc == 0:
+            print("[MQTT] connectedn as listener to mqtt server {}:{} on {}".format(self.broker, self.port, self.topic))
+            # start listening
+            self.client.loop_start()
+        else:
+            print("[MQTT] error while disconne cting")
+
+    def on_disconnect(self, userdata, rc):
+        print[f"[MQTT] disconnected: {str(rc)}"]
+        print("[MQTT] trying to reconnect ...")
+        self.client.reconnect()
+        
+    def on_connect(self, client, userdata, flags, rc):
+        client.subscribe(self.topic)
+        return rc
+
+    def on_message(self, client, userdata, msg):
+        self.function(client, userdata, msg)
+
+if __name__ == "__main__":
+    def on_msg(client, userdata, msg):
+        print(msg.payload)
+
+    listener = MqttListener("127.0.0.1", 1883, "test/#", on_msg)
+    while input() != "q":
+        pass
